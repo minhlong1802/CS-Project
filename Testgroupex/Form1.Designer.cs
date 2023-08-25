5dded2d2705f0be687ece43d8a102ae2933387b3
minhlong1802 <137919484+minhlong1802@users.noreply.github.com>
2023-08-25 10:51:39 +0700

    first commit

---
+++ Testgroupex/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Testgroupex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(494, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(294, 226);
+            this.panel1.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 83);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(112, 16);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "(ngày-tháng-năm)";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(206, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Xóa";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(97, 192);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Cập nhật";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.SystemColors.Window;
+            this.button1.Location = new System.Drawing.Point(6, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Số lượng khách mời:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Địa điểm:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ngày tổ chức:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox4.Location = new System.Drawing.Point(142, 150);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(139, 22);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(142, 109);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(139, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(142, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(139, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(142, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên cô dâu chú rể:";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(13, -3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thông tin ";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.richTextBox1.Location = new System.Drawing.Point(494, 244);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(294, 194);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "                                                                                 " +
+    "                                        - Chỉ cần nhập tên cô dâu chú rể của tiệ" +
+    "c cưới để xóa";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(507, 241);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 16);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Ghi chú";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(476, 426);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(3, 39);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 16);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "(Nam-Nữ)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Testgroupex/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Testgroupex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadDataGridView();
+        }
+
+        private void Form1_load(object sender, EventArgs e)
+        {
+            this.FormClosing += Form1_FormClosing;
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //Bổ sung chức năng nâng cao
+            DialogResult result = MessageBox.Show("Bạn chắc chắn muốn thoát?", "Thông báo", MessageBoxButtons.YesNo);
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+        private DataTable dataTable = new DataTable();
+        private void LoadDataGridView()
+        {
+            dataTable.Columns.Add("Tên cô dâu chú rể", typeof(string));
+            dataTable.Columns.Add("Ngày tổ chức", typeof(string));
+            dataTable.Columns.Add("Địa điểm", typeof(string));
+            dataTable.Columns.Add("Số lượng khách mời", typeof(int));
+
+            dataGridView1.DataSource = dataTable;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a;
+            if (int.TryParse(textBox4.Text, out a))
+            {
+                dataTable.Rows.Add(textBox1.Text, textBox2.Text, textBox3.Text, a);
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                textBox1.Focus();
+                SortDataTableByWeddingDate();
+
+            }
+            else
+            {
+                MessageBox.Show("Invalid input for 'Số lượng khách mời'. Please enter a valid number.", "Warning");
+                textBox4.Text = "";
+                textBox4.Focus();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string a = textBox1.Text;
+            for (int i = dataTable.Rows.Count - 1; i >= 0; i--)
+            {
+                DataRow row = dataTable.Rows[i];
+                if (row["Tên cô dâu chú rể"].ToString().ToLower() == a.ToLower())
+                {
+                    DialogResult b = MessageBox.Show("Bạn có chắc chắn muốn xóa tiệc cưới này?", "Thông báo", MessageBoxButtons.YesNo);
+                    if (b == DialogResult.Yes)
+                    {
+                        dataTable.Rows.Remove(row);
+                        textBox1.Text = "";
+                        textBox1.Focus();
+                        break;
+                    }
+                    else
+                    {
+                        textBox1.Text = "";
+                        textBox1.Focus();
+                        break;
+                    }
+
+                }
+                else
+                {
+
+                    textBox1.Text = "";
+                    textBox1.Focus();
+
+
+                }
+            }
+        }
+        //Bổ sung chức năng nâng cao 
+        private void SortDataTableByWeddingDate()
+        {
+            DataView dv = dataTable.DefaultView;
+            dv.Sort = "Ngày tổ chức";
+
+            List<DataRow> rows = dv.ToTable().Rows.Cast<DataRow>().ToList();
+            rows.Sort(new CustomDateComparer());
+
+            dataTable.Clear();
+            foreach (DataRow row in rows)
+            {
+                dataTable.Rows.Add(row.ItemArray);
+            }
+
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string userInputStr = InputDialog.ShowDialog("Bạn muốn cập nhật dòng nào: ", "Thông báo");
+
+            if (int.TryParse(userInputStr, out int userInput) && userInput >= 1 && userInput <= dataTable.Rows.Count)
+            {
+                DataRow row = dataTable.Rows[userInput - 1];
+
+                int choice = -1;
+                while (choice != 5)
+                {
+                    choice = int.Parse(InputDialog.ShowDialog(@"Lựa chọn:
+1. Tên cô dâu chú rể
+2. Ngày tổ chức
+3. Địa điểm
+4. Số lượng khách mời
+5. Hoàn thành cập nhật", "Thông báo"));
+
+                    switch (choice)
+                    {
+                        case 1:
+                            string newName = InputDialog.ShowDialog("Nhập tên cô dâu chú rể mới: ", "Cập nhật");
+                            row["Tên cô dâu chú rể"] = newName;
+                            break;
+                        case 2:
+                            string newDate = InputDialog.ShowDialog("Nhập ngày tổ chức mới [ngày-tháng-năm]: ", "Cập nhật");
+                            row["Ngày tổ chức"] = newDate;
+                            break;
+                        case 3:
+                            string newLocation = InputDialog.ShowDialog("Nhập địa điểm mới: ", "Cập nhật");
+                            row["Địa điểm"] = newLocation;
+                            break;
+                        case 4:
+                            string newNumberGuest = InputDialog.ShowDialog("Nhập số lượng khách mời mới: ", "Cập nhật");
+                            if (int.TryParse(newNumberGuest, out int numberGuest))
+                            {
+                                row["Số lượng khách mời"] = numberGuest;
+                            }
+                            else
+                            {
+                                MessageBox.Show("Invalid input for 'Số lượng khách mời'. Please enter a valid number.", "Warning");
+                            }
+                            break;
+                        case 5:
+                            SortDataTableByWeddingDate(); // Sort the table after completing updates
+                            break;
+                        default:
+                            MessageBox.Show("Invalid choice, please enter again!", "Thông báo");
+                            break;
+                    }
+                }
+
+                SortDataTableByWeddingDate(); 
+            }
+            else if (!string.IsNullOrEmpty(userInputStr)) 
+            {
+                MessageBox.Show("Invalid input. Please enter a valid row number.", "Thông báo");
+            }
+        }
+
+
+
+
+    }
+    class CustomDateComparer : IComparer<DataRow>
+    {
+        public int Compare(DataRow x, DataRow y)
+        {
+            DateTime date1 = DateTime.ParseExact(x["Ngày tổ chức"].ToString(), "dd-MM-yyyy", CultureInfo.InvariantCulture);
+            DateTime date2 = DateTime.ParseExact(y["Ngày tổ chức"].ToString(), "dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+            int yearComparison = date1.Year.CompareTo(date2.Year);
+            if (yearComparison != 0)
+            {
+                return yearComparison;
+            }
+
+            int monthComparison = date1.Month.CompareTo(date2.Month);
+            if (monthComparison != 0)
+            {
+                return monthComparison;
+            }
+
+            return date1.Day.CompareTo(date2.Day);
+        }
+    }
+
+
+
+    static class InputDialog
+    {
+        private static string inputValue = "";
+
+        public static string ShowDialog(string prompt, string title)
+        {
+            int formWidth = 400;  
+            int formHeight = 200; 
+
+            Form promptForm = new Form()
+            {
+                Width = formWidth,
+                Height = formHeight,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = title,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+
+            Label promptLabel = new Label()
+            {
+                Left = 20,       
+                Top = 5,       
+                Width = formWidth - 40,    
+                Height = formHeight - 80,  
+                Text = prompt
+            };
+
+            TextBox textBox = new TextBox()
+            {
+                Left = 20,       
+                Top = 85,        
+                Width = formWidth - 40
+            };
+
+            Button confirmation = new Button()
+            {
+                Text = "OK",
+                Left = formWidth / 2 - 30,
+                Width = 60,
+                Top = formHeight - 80 
+            };
+
+            confirmation.Click += (sender, e) =>
+            {
+                inputValue = textBox.Text;
+                promptForm.Close();
+            };
+
+            textBox.TextChanged += (sender, e) =>
+            {
+                inputValue = textBox.Text;
+            };
+
+            promptForm.Controls.Add(textBox);
+            promptForm.Controls.Add(confirmation);
+            promptForm.Controls.Add(promptLabel);
+            promptForm.AcceptButton = confirmation;
+
+            promptForm.ShowDialog();
+
+            return inputValue;
+        }
+    }
+}
+
+
